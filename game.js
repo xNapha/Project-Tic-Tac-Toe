520e4567fade0ae1250dcfcc2685af172d87b5ba
NathanJCai <nathancai1997@gmail.com>
2022-10-03 21:44:59 +1100

    Minor Update
    -removed some css elements
    -added a check for draw function
    - removed some buttons in html

--- game.js
+++ game.js
@@ -15,23 +15,27 @@ const gameBoard = (() =>{
         playerTwo: 0
     };
     
+    var startingPlayer = 0;
     var playerTurn = [];
-    var counter = 0;
+    var turnCounter = 0;
+    var pieceCounter = 0;
     
     
     const playerRotation = (player1, player2) =>{
-            for(let i = 0; i<9; i++){
-                if(i%2 ==0){
-                    playerTurn[i] = player1
-                } else{
-                    playerTurn[i] = player2
+            for(let i = 0; i < 11; i++){
+                if(i % 2 == 0){
+                    playerTurn[i] = player1;
+                } else {
+                    playerTurn[i] = player2;
                 };
             };
             createBoard()
-            addEvent(playerTurn[counter]);
+            addEvent(playerTurn[turnCounter]);
+            return;
         };
 
     const createBoard = () =>{
+        console.log(boardState);
         Object.entries(boardState).forEach(([key,value])=>{
             const img = document.createElement("img");
             const markerCon = document.querySelector("#"+key.toString()+"Con")
@@ -40,6 +44,7 @@ const gameBoard = (() =>{
             img.setAttribute("src", "./assets/" + value.toString() + ".png")
             markerCon.appendChild(img)
         });
+        return;
     };
 
     const addEvent = (x) => {
@@ -48,7 +53,7 @@ const gameBoard = (() =>{
                 const current = document.querySelector("#"+key.toString());
                 const squares = document.querySelectorAll(".marker");
                 current.addEventListener("click", () =>{
-                    boardState[key] = x
+                    boardState[key] = x;
                     squares.forEach(square =>{
                         square.remove();
                     });
@@ -60,81 +65,91 @@ const gameBoard = (() =>{
                 return;
             };
         });
+        return;
     };
 
     const checkWin = () =>{
-        const playerOneWin = [playerTurn[0], playerTurn[0],playerTurn[0]]
-        const playerTwoWin = [playerTurn[1],playerTurn[1],playerTurn[1]]
-        const winConOne = [boardState.topLeft, boardState.topMiddle, boardState.topRight]
-        const winConTwo = [boardState.left, boardState.middle, boardState.right]
-        const winConThree = [boardState.botLeft, boardState.botMiddle, boardState.botRight]
-        const winConFour = [boardState.topLeft, boardState.middle, boardState.botRight]
-        const winConFive = [boardState.botLeft, boardState.middle, boardState.topRight]
-        const winConSix = [boardState.topLeft, boardState.left, boardState.botLeft]
-        const winConSeven = [boardState.topMiddle, boardState.middle, boardState.botMiddle]
-        const winConEight = [boardState.topRight, boardState.right, boardState.botRight]
+        const playerOneWin = [playerTurn[0], playerTurn[0],playerTurn[0]];
+        const playerTwoWin = [playerTurn[1],playerTurn[1],playerTurn[1]];
+        const winConOne = [boardState.topLeft, boardState.topMiddle, boardState.topRight];
+        const winConTwo = [boardState.left, boardState.middle, boardState.right];
+        const winConThree = [boardState.botLeft, boardState.botMiddle, boardState.botRight];
+        const winConFour = [boardState.topLeft, boardState.middle, boardState.botRight];
+        const winConFive = [boardState.botLeft, boardState.middle, boardState.topRight];
+        const winConSix = [boardState.topLeft, boardState.left, boardState.botLeft];
+        const winConSeven = [boardState.topMiddle, boardState.middle, boardState.botMiddle];
+        const winConEight = [boardState.topRight, boardState.right, boardState.botRight];
 
         const winCons = [winConOne.join(""), winConTwo.join(""), winConThree.join(""), winConFour.join(""), winConFive.join(""), winConSix.join(""), winConSeven.join(""), winConEight.join("")]
-        
         for (let i = 0; i< winCons.length; i++){
             if(winCons[i] == playerOneWin.join("")){
                 score.playerOne++;
                 const playerOneScores = document.querySelector("#player1Scores");
                 playerOneScores.textContent = score.playerOne;
-                counter = 0;
-                displayController.newRound()
+                playerTwoFirst();
                 return;
             } else if (winCons[i] == playerTwoWin.join("")){
                 score.playerTwo++;
                 const playerTwoScores = document.querySelector("#player2Scores");
                 playerTwoScores.textContent = score.playerTwo;
-                counter = 0;
-                displayController.newRound()
+                playerOneFirst();
                 return;
             };
         };
-        counter++;
-        addEvent(playerTurn[counter]);
+        pieceCounter++;
+        if(pieceCounter == 9 && startingPlayer == 0){
+            console.log("player 1 test")
+            playerTwoFirst();
+            return;
+        } else if(pieceCounter == 9 && startingPlayer == 1){
+            console.log("player 2 test")
+            playerOneFirst();
+            return;
+        };
+        turnCounter++;
+        addEvent(playerTurn[turnCounter]);
     };
 
-    return {boardState ,score, playerRotation, createBoard, addEvent, playerTurn, counter}
-
-})();
-
-const displayController = (() =>{
-    const singlePlayer= () => {
-
-        return;
-    };
-
-    const twoPlayer= () => {
-        const playerCount = document.querySelector("#playerCount");
-        
-        return playerCount.setAttribute("z-index", "0");
-    };
-
-    const markerOption = () => {
-        return;
-    };
-
-    const newRound = () => {
+    const playerOneFirst = () => {
         const newBoard = document.querySelector("#newBoard");
-        const button = document.querySelector("button")
-        button.setAttribute("type", "button")
+        const button = document.createElement("button");
+        newBoard.append(button);
+        button.setAttribute("type", "button");
         button.textContent = "Play again?";
+        pieceCounter = 0;
+        turnCounter = 0;
+        startingPlayer = 0;
         button.addEventListener("click", ()=>{
-            Object.keys(gameBoard.boardState).forEach(key =>{
-                gameBoard.boardState[key] = "empty"
+            Object.keys(boardState).forEach(key =>{
+                boardState[key] = "empty";
             })
-            gameBoard.createBoard;
-            gameBoard.addEvent(gameBoard.playerTurn[0])
-            button.remove()
+            createBoard()
+            addEvent(playerTurn[turnCounter]);
+            button.remove();
         });
-        newBoard.append(button)
-        
-    }
+        return;
+    };
 
-    return {singlePlayer, twoPlayer, newRound}
+    const playerTwoFirst = () => {
+        const newBoard = document.querySelector("#newBoard");
+        const button = document.createElement("button");
+        newBoard.append(button);
+        button.setAttribute("type", "button");
+        button.textContent = "Play again?";
+        pieceCounter = 0;
+        turnCounter = 1;
+        startingPlayer = 1;
+        button.addEventListener("click", ()=>{
+            Object.keys(boardState).forEach(key =>{
+                boardState[key] = "empty";
+            })
+            createBoard()
+            addEvent(playerTurn[turnCounter]);
+            button.remove();
+        });
+        return;
+    };
+    return {playerRotation}
 })();
 
 const Player = (marker) =>{
@@ -142,6 +157,3 @@ const Player = (marker) =>{
 
     return {gamePiece};
 };
-
-const crosses = Player("crosses");
-const noughts = Player("noughts");
